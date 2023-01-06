0e7f476dd69b1b0ee5b549439e3fbe13c58c2477
MaryJose00 <108303677+MaryJose00@users.noreply.github.com>
2023-01-06 06:22:54 -0500

    Add files via upload

---
+++ prueba.bat
@@ -0,0 +1,4 @@
+java -Dserver.port=8080 -cp lib/* com.distribuida.Servidor
+java -Dserver.port=8081 -cp lib/* com.distribuida.Servidor
+java -Dserver.port=8082 -cp lib/* com.distribuida.Servidor
+java -Dserver.port=8083 -cp lib/* com.distribuida.Servidor
